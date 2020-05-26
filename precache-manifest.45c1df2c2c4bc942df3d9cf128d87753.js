59c2dfd43dd2064d47e41234720f29ddeee34a0d
garvitchittora <garvitchittora2016@gmail.com>
2020-05-26 20:33:22 +0530

    Updates

---
+++ precache-manifest.45c1df2c2c4bc942df3d9cf128d87753.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fe4960237f1d8b151a22e7355eec30f",
+    "url": "/garvitchittora-SGR3-moviesy/index.html"
+  },
+  {
+    "revision": "11ed9d3099d276bee3c8",
+    "url": "/garvitchittora-SGR3-moviesy/static/css/2.4c6ee7a4.chunk.css"
+  },
+  {
+    "revision": "403c0b923197f0d755a9",
+    "url": "/garvitchittora-SGR3-moviesy/static/css/main.da096839.chunk.css"
+  },
+  {
+    "revision": "11ed9d3099d276bee3c8",
+    "url": "/garvitchittora-SGR3-moviesy/static/js/2.81086a4b.chunk.js"
+  },
+  {
+    "revision": "4386012a6d334684338618496b7aac20",
+    "url": "/garvitchittora-SGR3-moviesy/static/js/2.81086a4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "403c0b923197f0d755a9",
+    "url": "/garvitchittora-SGR3-moviesy/static/js/main.563e257d.chunk.js"
+  },
+  {
+    "revision": "d9e49512eb86474c6e73",
+    "url": "/garvitchittora-SGR3-moviesy/static/js/runtime-main.646d73d9.js"
+  }
+]);
